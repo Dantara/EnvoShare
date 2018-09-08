d9e303c6b704f8028207cc606149d3c7fbb0bd71
Dantara <supercreed2@gmail.com>
2018-09-08 18:02:44 +0300

    Docker related changes

--- lib/envshare/docker.ex
+++ lib/envshare/docker.ex
@@ -1,7 +1,7 @@
 defmodule Docker do
   def make_dockerfile(commands) do
     content = Enum.join(commands, "\n")
-    Path.expand('./docker/Makefile') |> Path.absname |> File.write(content, [:write])
+    Path.expand('./docker/Dockerfile') |> Path.absname |> File.write(content, [:write])
 
     :ok
   end
